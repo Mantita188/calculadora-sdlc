dacc1631eafa9609dac8a12e02c7fb6ac0fc0623
Mantita188 <conita551@gmail.com>
2025-09-24 00:32:52 -0300

    feat: se crea la estructura inicial y menú principal para entender lo que se debe hacer :).

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+#Proyecto creacion de una calculadora :D
+while True:
+    print("Menú de la Calculadora:")
+    print("a) Sumar (proximamente)")
+    print("b)Restar (proximamente)")
+    print("c)Multiplicar (proximamente)")
+    print("d)Dividir (proximamente)")
+    print("e) Salir")
+    opcion = input("Elige la operación que quiere realizar a continuacion: ").lower()
